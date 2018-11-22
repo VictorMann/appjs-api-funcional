8b330f5443983c9662205e47cd8adedb8ddcda62
Victor Mann <victorcmann@gmail.com>
2018-11-22 16:23:48 -0200

    aplicando log na requisicao

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,16 @@
-import {handleStatus} from './utils/promises-helpers.js';
+import {handleStatus, log} from './utils/promises-helpers.js';
 
 document
 .querySelector('#myButton')
 .addEventListener('click', () => 
     fetch('/notas')
     .then(handleStatus)
+    .then(log)
+    .then(notas => notas.reduce((arr, nota) => arr.concat(nota.itens), []))
+    .then(log)
+    .then(itens => itens.filter(item => item.codigo == 2143))
+    .then(log)
+    .then(itens => itens.reduce((total, item) => total + item.valor, 0))
     .then(console.log)
     .catch(console.log)
 );
