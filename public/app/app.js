e964ef61f39a70d8a0ccd5e1c2e627e39dd7f169
Victor Mann <victorcmann@gmail.com>
2018-11-23 18:31:31 -0200

    partializando takeUtil e debounce

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,18 @@
 import { log } from './utils/promises-helpers.js';
 import './utils/array-helpers.js';
 import { notaService as service } from './nota/service.js';
-import { takeUtil, debounce } from './utils/operators.js';
+import { takeUtil, debounce, pipe, partialize } from './utils/operators.js';
 
-const action = debounce(500,
-    takeUtil(3, () => 
-        service
-        .sumItems(2143)
-        .then(console.log)
-        .catch(console.log)
-    )
+const operation = pipe(
+    partialize(takeUtil, 3),
+    partialize(debounce, 500)
+);
+
+const action = operation(() =>
+    service
+    .sumItems(2143)
+    .then(console.log)
+    .catch(console.log)
 );
 
 document
