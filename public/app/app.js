071bdbd6a2d24809d02419eacc3f32f1046d6c8e
Victor Mann <victorcmann@gmail.com>
2018-11-24 19:31:13 -0200

    emitindo o total para observadores com EventEmitter

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,7 @@ import { log, timeoutPromise, retry } from './utils/promises-helpers.js';
 import './utils/array-helpers.js';
 import { notaService as service } from './nota/service.js';
 import { takeUtil, debounce, pipe, partialize } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operation = pipe(
     partialize(takeUtil, 3),
@@ -10,7 +11,7 @@ const operation = pipe(
 
 const action = operation(() =>
     retry(3, 3000, () => timeoutPromise(200, service.sumItems(2143)) )
-    .then(console.log)
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
 
